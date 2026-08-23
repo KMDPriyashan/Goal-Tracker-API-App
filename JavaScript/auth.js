571384163a452dadb546b94937247a14c1279aad
K.M.D.Priyashan <kmdpriyashanofficials2001@gmail.com>
2026-08-22 22:07:46 -0400

    auth.js file creation with in login and logout function. 🚩

--- JavaScript/auth.js
+++ JavaScript/auth.js
@@ -1,10 +1,10 @@
 //register new user
-async function registerUser(userData){
+async function registerUser(userData) {
     const result = await apiRequest('/register', 'POST', userData);
 
-    if(result.result === true){
+    if (result.result === true) {
         showAlert('success', 'registration successfully please login');
-    }else{
+    } else {
         showAlert('danger', result.message || 'registration failed !');
     }
 
@@ -12,3 +12,29 @@ async function registerUser(userData){
 }
 
 //login user
+async function loginUser(credentials) {
+    const result = await apiRequest('/login', 'POST', credentials);
+
+    if (result.result === true) {
+        sessionStorage.setItem('user', JSON.stringify(result.data));
+        showAlert('success', 'login Successful;ly...');
+    } else {
+        showAlert('danger', result.message || 'login failed');
+    }
+    return result;
+}
+
+//logout user
+function logoutUser() {
+    sessionStorage.removeItem('user');
+    window.location.href = 'login.html';
+}
+
+// Check authentication status
+function checkAuth() {
+    if (!isLoggedIn()) {
+        window.location.href = 'login.html';
+        return false;
+    }
+    return true;
+}
